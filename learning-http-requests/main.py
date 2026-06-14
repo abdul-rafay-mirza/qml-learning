eafd64ec37647d49f63512a369f736c915fd8653
abdul-rafay-mirza <246260926+abdul-rafay-mirza@users.noreply.github.com>
2026-06-14 18:00:27 +0500

    Almost finish the anilist anime page query, Recommendations left

--- learning-http-requests/main.py
+++ learning-http-requests/main.py
@@ -5,19 +5,114 @@ url = "https://graphql.anilist.co"
 
 query = '''
 query ($id: Int) {
-  Media (id: $id, type: ANIME) {
+  Media(id: $id, type: ANIME) {
     id
     title {
       romaji
       english
       native
     }
+    nextAiringEpisode {
+      airingAt
+      timeUntilAiring
+      episode
+    }
+    format
+    episodes
+    duration
+    status
+    startDate {
+      day
+      month
+      year
+    }
+    endDate {
+      day
+      month
+      year
+    }
+    averageScore
+    meanScore
+    popularity
+    favourites
+    studios {
+      nodes {
+        name
+        isAnimationStudio
+      }
+    }
+    source
+    hashtag
+    genres
+    synonyms
+    tags {
+      name
+      rank
+      isMediaSpoiler
+    }
+    externalLinks {
+      site
+      url
+    }
+    bannerImage
+    coverImage {
+      large
+    }
+    description
+    relations {
+      edges {
+        relationType(version: 2)
+        node {
+          id
+          type
+          format
+          title {
+            romaji
+            english
+          }
+          coverImage {
+            large
+          }
+          status
+        }
+      }
+    }
+    characters(sort: [FAVOURITES_DESC, ROLE], perPage: 6) {
+      edges {
+        role
+        node {
+          id
+          name {
+            full
+            native
+          }
+          image {
+            large
+          }
+        }
+      }
+    }
+    staff(sort: [FAVOURITES_DESC, ROLE], perPage: 6) {
+      edges {
+        role
+        node {
+          id
+          name {
+            full
+            native
+          }
+          image {
+            large
+          }
+        }
+      }
+    }
   }
 }
 '''
 
 variables = {
-    'id': 189046
+    'id': 147105
 }
 
 dictToPass = {
@@ -27,10 +122,10 @@ dictToPass = {
 
 response = requests.post(url, json=dictToPass)
 
-to_dict: dict = json.loads(response.text)
+json_object: dict = response.json()
 
 myFile = open("anime_request.json", "w")
-json.dump(to_dict, myFile, indent = 4)
+json.dump(response.json(), myFile, indent = 4)
 myFile.close()
 
-print(to_dict["data"]["Media"]["title"]["romaji"])
+print(json_object["data"]["Media"]["title"]["romaji"])
